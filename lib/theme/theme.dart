408aefcf42790071e2e1ffaedb4faedf8d5608c3
smit <monparasmit1@gmail.com>
2022-09-22 14:47:00 +0530

    formate code

--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class MyTheme {
   static ThemeData lightTheme() => ThemeData(
-    scaffoldBackgroundColor: Colors.white,
+      scaffoldBackgroundColor: Colors.white,
       textTheme: const TextTheme(
           headline1: TextStyle(
               fontSize: 30, fontWeight: FontWeight.bold, color: Colors.black),
